3822da438ef5687be1944e5a0fadf1eb480b0c54
aminelch <aminelch95@gmail.com>
2021-03-14 03:47:31 +0100

    files dirs owners

--- file-permission-acl/chmod-example.zsh
+++ file-permission-acl/chmod-example.zsh
@@ -5,3 +5,12 @@ ls afile
 
 #Set the same permission using a symbolic specification, using the verbose option:
 chmod -v u=rw,go=r afile 
+
+#Recursivly remove all permissions for other on a directory 
+chmod -R -v o-rwx testdir/ 
+
+#Recursivly add rx permissions for other on a directory 
+chmod -R -v o+rx testdir 
+
+#Set the sticky bit on a directory:
+chmod -v +t adir
---
+++ file-permission-acl/chown-example.zsh
@@ -0,0 +1,7 @@
+#!/bin/zsh 
+
+# set the user owner of a file 
+chown -v jdoe afile
+
+# set the user and group owner of file 
+chown -v joe.sales afile 
